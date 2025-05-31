788d71398557773feb9a529334c3ed3375483ab9
sidh3369 <69889994+sidh3369@users.noreply.github.com>
2025-05-31 21:55:12 +0530

    Delete netlify/functions directory

--- netlify/functions/update-playlist.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-    const formData = new URLSearchParams(event.body);
-    const playlist = formData.get('playlist');
-    // Save the playlist link or file for later use
-    return {
-        statusCode: 302,
-        headers: { 'Location': '/' }
-    };
-};
